ee99567300e11be8599f6e9321d4971f9f83a75e
Arushi Bose <arushi.bose@connect.qut.edu.au>
2023-10-31 01:34:33 +1000

    Make change to main.py

--- a3_group16/main.py
+++ a3_group16/main.py
@@ -2,5 +2,5 @@ from a3_group16 import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run()
+    app.run(debug=True, port=5001)
     #disable debug mode once successfully deployed
