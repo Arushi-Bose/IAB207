36738e86ce156d405d474894c674d4b430114013
Bunji75 <DayneLinke@gmail.com>
2023-10-31 12:17:08 +1000

    Implemented login and register functions

--- a3_group16/BeatBazaar/models.py
+++ a3_group16/BeatBazaar/models.py
@@ -1,7 +1,7 @@
 from flask_login import UserMixin
 from . import db
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users' # good practice to specify table name
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), index=True, unique=True, nullable=False)
@@ -9,7 +9,7 @@ class User(db.Model):
     # Password needs to be encrypted before being stored in the db
     password_hash = db.Column(db.String(255), nullable=False)
     # The comment relation will only be used if we need to link users and comments
-    comments = db.relationship('Comment', backref='user')
+    # comments = db.relationship('Comment', backref='user')
     
     # string print method
     def __repr__(self):
