80ef80bce62c9c71011d08f2158f665216faf812
Bunji75 <DayneLinke@gmail.com>
2023-10-31 12:41:38 +1000

    Added minimum length for passwords
    Added a minimum length of passwords and also added in some of the routes for the other pages

--- a3_group16/BeatBazaar/views.py
+++ a3_group16/BeatBazaar/views.py
@@ -5,3 +5,11 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 def index():
     return render_template('index.html')
+
+@main_bp.route('/history')
+def history():
+    return render_template('booking-history.html')
+
+@main_bp.route('/create')
+def create_event():
+    return render_template('event-creation-update.html')
