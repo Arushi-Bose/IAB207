d76271ea1ecbbbb5e1474c085426fa499ce892ed
Bunji75 <DayneLinke@gmail.com>
2023-11-02 15:49:43 +1000

    Working through booking history functionality

--- a3_group16/BeatBazaar/views.py
+++ a3_group16/BeatBazaar/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required
-from .models import Event
+from .models import Event, Bookings
 from . import db
 
 main_bp = Blueprint('main', __name__)
@@ -15,7 +15,9 @@ def filter(country):
         events = db.session.scalars(db.select(Event).where(Event.country.like(country)))
         return render_template('index.html', events=events)
 
-@main_bp.route('/history')
+@main_bp.route('/history/<id>')
 @login_required
-def history():
-    return render_template('booking-history.html')
+def history(id):
+ 
+    bookings = Bookings.query.join(Event, Bookings.events_id==Event.id).filter(Bookings.user_id==id).order_by(Bookings.booking_number)
+    return render_template('booking-history.html', bookings=bookings)
